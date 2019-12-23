7b3fb3c1547477c5e60270f6c3c4c5385142b2a0
Gian Lazzarini <gianlazzarini@gmail.com>
2019-12-23 15:44:30 -0800

    Nestjs backend now working with cors

---
+++ back-end/src/config/configuration.ts
@@ -0,0 +1,10 @@
+export default () => ({
+    // port: parseInt(process.env.PORT, 10) || 3000,
+    // database: {
+    //   host: process.env.DATABASE_HOST,
+    //   port: parseInt(process.env.DATABASE_PORT, 10) || 5432
+    // },
+    ACCESS_TOKEN_SECRET: "SecretKey",
+    FIREBASE_SERVER_KEY: "AAAAs2vRI3U:APA91bFEanHjh482pnvKSWGAg7o2Kzd0GCqhobUTJ2yyhIQ-swlkZ2Sv2ADuoHJUVCbY308Ezd-g9ZjR35QgxP0FRLxeJGSJkmGX3Sub4SlKXHZt73QN7s2ETklgSedB_oz9m0CAoJPV",
+    APP_NAME: "Lazztech Hub",
+  });
