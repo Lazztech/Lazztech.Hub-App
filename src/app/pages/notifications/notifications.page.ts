df31ca49731fac3c901d0a0032ddd74ad9e0dc23
Leo Lazzarini <leolazzarini101@gmail.com>
2021-11-10 18:04:35 -0800

    Implemented infinite scroll

--- src/app/pages/notifications/notifications.page.ts
+++ src/app/pages/notifications/notifications.page.ts
@@ -1,21 +1,20 @@
-import { Component, OnInit, OnDestroy } from "@angular/core";
-import { NotificationsService } from "src/app/services/notifications/notifications.service";
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { NotificationsService } from 'src/app/services/notifications/notifications.service';
 import {
   InAppNotification,
   GetInAppNotificationsQuery,
   Exact,
-} from "src/generated/graphql";
-import { NGXLogger } from "ngx-logger";
-import { Observable, Subscription } from "rxjs";
-import { map } from "rxjs/operators";
-import { NavController } from "@ionic/angular";
-import { QueryRef } from "apollo-angular";
-import { ApolloQueryResult } from "apollo-client";
+} from 'src/generated/graphql';
+import { NGXLogger } from 'ngx-logger';
+import { Observable, Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { NavController } from '@ionic/angular';
+import { QueryRef } from 'apollo-angular';
 
 @Component({
-  selector: "app-notifications",
-  templateUrl: "./notifications.page.html",
-  styleUrls: ["./notifications.page.scss"],
+  selector: 'app-notifications',
+  templateUrl: './notifications.page.html',
+  styleUrls: ['./notifications.page.scss'],
 })
 export class NotificationsPage implements OnInit, OnDestroy {
   loading = true;
@@ -28,12 +27,10 @@ export class NotificationsPage implements OnInit, OnDestroy {
     }>
   >;
   subscriptions: Subscription[] = [];
-  limit: number = 10;
-  offset: number = 0;
-
-  sortedInAppNotifications: GetInAppNotificationsQuery["getInAppNotifications"] =
+  limit = 10;
+  offset = 0;
+  sortedInAppNotifications: GetInAppNotificationsQuery['getInAppNotifications'] =
     [];
-
   constructor(
     private notificationsService: NotificationsService,
     private logger: NGXLogger,
@@ -50,7 +47,7 @@ export class NotificationsPage implements OnInit, OnDestroy {
 
     this.subscriptions.push(
       this.queryRef.valueChanges.subscribe((result) => {
-        this.logger.log("loading: ", result.loading);
+        this.logger.log('loading: ', result.loading);
         this.loading = result.loading;
         const combinedResults = this.sortedInAppNotifications.concat(
           result?.data?.getInAppNotifications
@@ -58,17 +55,17 @@ export class NotificationsPage implements OnInit, OnDestroy {
         this.sortedInAppNotifications = this.sortNotifications(combinedResults);
       })
     );
-
-    // const inAppNotifications = this.notificationsService
-    // .watchGetInAppNotifications(null, 5000)
-    // .valueChanges.pipe(map((x) => {
-    //   this.loading = x.loading;
-    //   return this.sortNotifications(x?.data?.getInAppNotifications);
-    // }));
   }
 
   loadData(event) {
-    // this.inAppNotifications;
+    this.loadMore().then((x) => {
+      const data = x?.data?.getInAppNotifications;
+      this.sortedInAppNotifications = this.sortNotifications(
+        this.sortedInAppNotifications.concat(data)
+      );
+      event.target.complete();
+    });
+    console.log(this.sortedInAppNotifications.length);
   }
 
   ngOnDestroy() {
@@ -104,11 +101,15 @@ export class NotificationsPage implements OnInit, OnDestroy {
 
   async doRefresh(event) {
     try {
-      let moreNotifications = (await this.loadMore()).data
-        ?.getInAppNotifications;
       this.sortedInAppNotifications = this.sortNotifications(
-        this.sortedInAppNotifications.concat(moreNotifications)
+        (await this.queryRef.refetch({ limit: this.limit, offset: 0 })).data
+          ?.getInAppNotifications
       );
+      // let moreNotifications = (await this.loadMore()).data
+      //   ?.getInAppNotifications;
+      // this.sortedInAppNotifications = this.sortNotifications(
+      //   this.sortedInAppNotifications.concat(moreNotifications)
+      // );
       this.loading = false;
       event.target.complete();
     } catch (error) {
@@ -118,7 +119,7 @@ export class NotificationsPage implements OnInit, OnDestroy {
   }
 
   async deleteNotifications() {
-    const result = confirm("Delete all notifications?");
+    const result = confirm('Delete all notifications?');
     if (result) {
       await this.notificationsService.deleteAllInAppNotifications();
     }
