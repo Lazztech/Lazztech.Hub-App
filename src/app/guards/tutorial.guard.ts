4c474f6cc35770189664adcfdfdd6846ed019298
Gian Lazzarini <gianlazzarini@gmail.com>
2022-05-19 20:40:09 -0700

    Adjusted tutorial guard to ensure is authed

--- src/app/guards/tutorial.guard.ts
+++ src/app/guards/tutorial.guard.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
 import { Storage } from '@ionic/storage';
+import { AuthService } from '../services/auth/auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,14 +11,15 @@ export class TutorialGuard implements CanActivate {
   constructor(
     private storage: Storage,
     private router: Router,
+    private authService: AuthService,
   ) { }
 
   async canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Promise<boolean> {
     const isComplete = await this.storage.get('tutorialCompleted');
-
-    if (!isComplete) {
+    const isAuthed = await this.authService.getToken();
+    if (!isComplete && isAuthed) {
       this.router.navigateByUrl('tutorial');
     }
 
