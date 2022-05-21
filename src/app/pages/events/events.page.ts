8958a35172395aa44d9871b3d60f768733e1416e
Gian Lazzarini <gianlazzarini@gmail.com>
2022-05-21 14:53:23 -0700

    mocked fab button for adding event on events page

--- src/app/pages/events/events.page.ts
+++ src/app/pages/events/events.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-events',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventsPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    public navCtrl: NavController,
+  ) { }
 
   ngOnInit() {
+  }
+
+  goToCreateEventPage() {
+    this.navCtrl.navigateForward('');
   }
 
 }
