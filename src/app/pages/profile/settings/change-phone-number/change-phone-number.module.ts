7273f74b254f1f13cf6e7a2574ba2f83159c82e2
Gian Lazzarini <gianlazzarini@gmail.com>
2022-08-07 21:09:02 -0700

    stubbing change phone number page

--- src/app/pages/profile/settings/change-phone-number/change-phone-number.module.ts
+++ src/app/pages/profile/settings/change-phone-number/change-phone-number.module.ts
@@ -1,19 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
 import { ChangePhoneNumberPageRoutingModule } from './change-phone-number-routing.module';
 
 import { ChangePhoneNumberPage } from './change-phone-number.page';
+import { PipesModule } from 'src/app/pipes/pipes.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    ChangePhoneNumberPageRoutingModule
+    ChangePhoneNumberPageRoutingModule,
+    ReactiveFormsModule,
+    PipesModule,
   ],
   declarations: [ChangePhoneNumberPage]
 })
--- src/app/pages/profile/settings/change-phone-number/change-phone-number.page.ts
+++ src/app/pages/profile/settings/change-phone-number/change-phone-number.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { AlertService } from 'src/app/services/alert/alert.service';
+import { CommunicationService } from 'src/app/services/communication.service';
+import { ProfileService } from 'src/app/services/profile/profile.service';
 
 @Component({
   selector: 'app-change-phone-number',
@@ -7,9 +11,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChangePhoneNumberPage implements OnInit {
 
-  constructor() { }
+  loading = false;
+
+  myForm: FormGroup;
+
+  get phoneNumber() {
+    return this.myForm.get('phoneNumber');
+  }
+  public countries: Array<{ code: number, flag: string, region: string}> = [];
+
+  constructor(
+    private fb: FormBuilder,
+    private profileService: ProfileService,
+    private alertService: AlertService,
+    private readonly communicationService: CommunicationService,
+  ) { }
 
   ngOnInit() {
+    this.countries = this.communicationService.countryCodes();
+    this.myForm = this.fb.group({
+      phoneNumber: []
+    });
+  }
+
+  async changePhoneNumber() {
+    this.loading = true;
+
+    const formValue = this.myForm.value;
+
+    const result = await this.profileService.changeEmail(formValue.phoneNumber);
+    if (result) {
+      this.loading = false;
+      this.alertService.presentToast('Changed phone number.');
+    } else {
+      this.loading = false;
+      this.alertService.presentRedToast('Failed to change phone number.');
+    }
   }
 
 }
