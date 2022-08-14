0b6e63c0891ecde0c69d34875441b3f32d747bcf
Gian Lazzarini <gianlazzarini@gmail.com>
2022-08-14 16:29:01 -0700

    clean up & add loader to events page

--- src/app/pages/events/events.page.ts
+++ src/app/pages/events/events.page.ts
@@ -11,22 +11,17 @@ import { JoinUserEvent, UserEventsGQL, UserEventsQuery } from 'src/generated/gra
 })
 export class EventsPage implements OnInit, OnDestroy {
 
-  demoEvent: JoinUserEvent = {
-    userId: "1",
-    eventId: "1",
-    event: {
-      id: "1",
-      name: "My First Demo Event",
-      shareableId: "1",
-      image: "https://lazztech-hub-service-z84zo.ondigitalocean.app/file/bb06c3e0-cd60-11ec-8708-ab51b4e23219.jpg",
-    }
-  }
-
   subscriptions: Subscription[] = [];
   userEventsQueryResult: ApolloQueryResult<UserEventsQuery>;
   sortedEvents: UserEventsQuery['usersEvents'];
   upcomingEvents: UserEventsQuery['usersEvents'];
   elapsedEvents: UserEventsQuery['usersEvents'];
+
+  public get loading() : boolean {
+    return [
+      this.userEventsQueryResult,
+    ].some(x => x?.loading);
+  }
 
   constructor(
     public navCtrl: NavController,
@@ -37,8 +32,8 @@ export class EventsPage implements OnInit, OnDestroy {
     this.subscriptions.push(
       this.userEvents.watch(null,{
         pollInterval: 2000,
-      }).valueChanges.subscribe(x => {
-        this.userEventsQueryResult = x;
+      }).valueChanges.subscribe(result => {
+        this.userEventsQueryResult = result;
         if (this.userEventsQueryResult?.data?.usersEvents) {
           this.sortedEvents = [...this.userEventsQueryResult?.data?.usersEvents]?.sort(
             (a, b) => new Date(b?.event?.startDateTime).valueOf() - new Date(a?.event?.startDateTime).valueOf()
