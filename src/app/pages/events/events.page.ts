8fcf9c546d97e4d9940fb1f230b0d822bebc5b21
Gian Lazzarini <gianlazzarini@gmail.com>
2022-08-14 16:38:54 -0700

    removed unused variable

--- src/app/pages/events/events.page.ts
+++ src/app/pages/events/events.page.ts
@@ -2,7 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ApolloQueryResult } from '@apollo/client/core';
 import { NavController } from '@ionic/angular';
 import { Subscription } from 'rxjs';
-import { JoinUserEvent, UserEventsGQL, UserEventsQuery } from 'src/generated/graphql';
+import { UserEventsGQL, UserEventsQuery } from 'src/generated/graphql';
 
 @Component({
   selector: 'app-events',
