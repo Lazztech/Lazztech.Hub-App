c91b7184a0ee5d4bd946cfcedb05608c70fed07a
Gian Lazzarini <gianlazzarini@gmail.com>
2022-11-06 13:41:37 -0800

    try catch error handling and UI handling for login

--- src/app/pages/auth/landing/landing.page.ts
+++ src/app/pages/auth/landing/landing.page.ts
@@ -75,14 +75,15 @@ export class LandingPage implements OnInit {
   async login() {
     this.loading = true;
     const formValue = this.myForm.value;
-    const token = await this.authService.login(formValue.email, formValue.password);
-    this.logger.log('Result: ' + token);
-    if (token) {
+    try {
+      const token = await this.authService.login(formValue.email, formValue.password);
+      if (token) {
+        this.loading = false;
+        await this.navCtrl.navigateRoot('/tabs');
+      }
+    } catch (error) {
+      this.alertService.presentRedToast(`Login failed: ${error}`, 5000);
       this.loading = false;
-      await this.navCtrl.navigateRoot('/tabs');
-    } else {
-      this.loading = false;
-      this.alertService.presentRedToast('Login failed!');
     }
   }
 
