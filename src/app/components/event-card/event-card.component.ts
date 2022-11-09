017637a0e0dd918324d40a91673a8b4b08af9e69
Gian Lazzarini <gianlazzarini@gmail.com>
2022-11-08 17:40:48 -0800

    fixed event card on hub page

--- src/app/components/event-card/event-card.component.ts
+++ src/app/components/event-card/event-card.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
-import { JoinUserEvent, RsvpGQL } from 'src/graphql/graphql';
+import { Event, JoinUserEvent, RsvpGQL } from 'src/graphql/graphql';
 
 @Component({
   selector: 'app-event-card',
@@ -9,6 +9,7 @@ import { JoinUserEvent, RsvpGQL } from 'src/graphql/graphql';
 })
 export class EventCardComponent implements OnInit {
 
+  @Input() event: Event;
   @Input() userEvent: JoinUserEvent;
   @Input() includeMap?: boolean = false;
   @Input() showRsvp?: boolean = false;
@@ -21,7 +22,7 @@ export class EventCardComponent implements OnInit {
   ngOnInit() {}
 
   goToEventPage() {
-    this.navCtrl.navigateForward('event/' + this.userEvent.eventId);
+    this.navCtrl.navigateForward('event/' + this.event?.id || this.userEvent?.eventId);
   }
 
   goToCreateEventPage() {
@@ -31,7 +32,7 @@ export class EventCardComponent implements OnInit {
   async segmentChanged(ev: any) {
     console.log('Segment changed', ev?.detail?.value);
     await this.rsvpService.mutate({
-      eventId: this.userEvent.eventId,
+      eventId: this.event?.id || this.userEvent?.eventId,
       rsvp: ev?.detail?.value
     }).toPromise();
   }
