f9b4342366e895a2e5a1dcc7b5eb70f09bebbb0a
Gian Lazzarini <gianlazzarini@gmail.com>
2022-11-16 18:25:14 -0800

    Search and polling (#42)
    * polling on people page
    
    * re-implemented hub search on home page
    
    * added search to events page
    
    * refining filterEvents method
    
    * disabled swipGesuture on split-pane ion-menu

--- src/app/pages/events/events.page.ts
+++ src/app/pages/events/events.page.ts
@@ -14,8 +14,9 @@ export class EventsPage implements OnInit, OnDestroy {
 
   subscriptions: Subscription[] = [];
   queryRefs: QueryRef<any>[] = [];
-
+  filter = '';
   userEventsQueryResult: ApolloQueryResult<UserEventsQuery>;
+  filteredEvents: UserEventsQuery['usersEvents'];
   sortedEvents: UserEventsQuery['usersEvents'];
   upcomingEvents: UserEventsQuery['usersEvents'];
   elapsedEvents: UserEventsQuery['usersEvents'];
@@ -66,6 +67,20 @@ export class EventsPage implements OnInit, OnDestroy {
 
   goToCreateEventPage() {
     this.navCtrl.navigateForward('create-event');
+  }
+
+  filterEvents(ev: any) {
+    this.filter = ev.target.value;
+    console.log('here')
+
+    if (this.filter && this.filter.trim() !== '') {
+      this.filteredEvents = this.sortedEvents?.filter(x => {
+        const name = x?.event?.name?.trim()?.toLowerCase();
+        return name?.includes(this.filter?.trim()?.toLowerCase());
+      }).sort(
+        (a, b) => new Date(b?.event?.startDateTime).valueOf() - new Date(a?.event?.startDateTime).valueOf()
+      );
+    }
   }
 
 }
