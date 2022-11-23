0e47d4b1c39e679596819568228b4fdd61cca533
Gian Lazzarini <gianlazzarini@gmail.com>
2022-11-23 14:31:54 -0800

    39 -> 40 passing, added missing imports & providers

--- src/app/pages/status/status.page.spec.ts
+++ src/app/pages/status/status.page.spec.ts
@@ -1,5 +1,9 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { Diagnostic } from '@awesome-cordova-plugins/diagnostic/ngx';
 import { IonicModule } from '@ionic/angular';
+import { ApolloTestingModule } from 'apollo-angular/testing';
+import { LoggerModule } from 'ngx-logger';
+import { environment } from 'src/environments/environment';
 
 import { StatusPage } from './status.page';
 
@@ -10,7 +14,14 @@ describe('StatusPage', () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ StatusPage ],
-      imports: [IonicModule.forRoot()]
+      imports: [
+        IonicModule.forRoot(),
+        LoggerModule.forRoot(environment.logging),
+        ApolloTestingModule,
+      ],
+      providers: [
+        Diagnostic,
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(StatusPage);
