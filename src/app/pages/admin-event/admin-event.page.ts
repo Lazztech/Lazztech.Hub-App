04b7fdcab4022c1814a22d2d21a9ef8bc817e489
Gian Lazzarini <gianlazzarini@gmail.com>
2023-01-02 15:11:51 -0800

    admin event page on startDateTime change set endDateTime to 1 hour after start

--- src/app/pages/admin-event/admin-event.page.ts
+++ src/app/pages/admin-event/admin-event.page.ts
@@ -186,6 +186,14 @@ export class AdminEventPage implements OnInit, OnDestroy {
     await actionSheet.present();
   }
 
+  startDateTimeChange(ev) {
+    const dateTime = new Date(ev?.detail?.value);
+    dateTime.setHours(dateTime.getHours() + 1);
+    this.myForm.patchValue({
+      endDateTime: dateTime,
+    });
+  }
+
   toggleStartDateTimeModal() {
     this.startDateTimeModalOpen = !this.startDateTimeModalOpen;
   }
