2ba40cd2a01613eaa9e127683a7c661f80cfeb37
Gian Lazzarini <gianlazzarini@gmail.com>
2023-01-05 15:35:30 -0800

    show code & scan code in qr page

--- src/app/pages/qr/qr.page.ts
+++ src/app/pages/qr/qr.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ScreenBrightness } from '@capacitor-community/screen-brightness';
+import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
 
 @Component({
   selector: 'app-qr',
@@ -9,16 +10,69 @@ import { ScreenBrightness } from '@capacitor-community/screen-brightness';
 export class QrPage implements OnInit, OnDestroy {
 
   initialScreenBrightness: number;
+  showCode: boolean = true;
 
   constructor() { }
 
   async ngOnInit() {
     this.initialScreenBrightness = (await ScreenBrightness.getBrightness()).brightness;
-    await ScreenBrightness.setBrightness({ brightness: 1 });
+    this.openCode();
   }
 
   async ngOnDestroy() {
     await ScreenBrightness.setBrightness({ brightness: this.initialScreenBrightness });
+    await this.closeScanner();
+  }
+
+  async segmentChanged(event) {
+    console.log(event)
+    const value = event?.detail?.value;
+    console.log(value)
+    switch (event?.detail?.value) {
+      case 'show-code':
+        await this.openCode()
+        await this.closeScanner();
+        break;
+      case 'scan-code':
+        await this.closeCode();
+        await this.openScanner();
+        break;
+      default:
+        break;
+    }
+  }
+
+  async openCode() {
+    this.showCode = true;
+    await ScreenBrightness.setBrightness({ brightness: 1 });
+  }
+
+  async closeCode() {
+    this.showCode = false;
+    await ScreenBrightness.setBrightness({ brightness: this.initialScreenBrightness });
+  }
+
+  async openScanner() {
+    // Check camera permission
+    // This is just a simple example, check out the better checks below
+    await BarcodeScanner.checkPermission({ force: true });
+
+    // make background of WebView transparent
+    // note: if you are using ionic this might not be enough, check below
+    BarcodeScanner.hideBackground();
+    document.querySelector('body').classList.add('scanner-active');
+
+    const result = await BarcodeScanner.startScan(); // start scanning and wait for a result
+
+    // if the result has content
+    if (result.hasContent) {
+      console.log(result.content); // log the raw scanned content
+    }
+  }
+
+  async closeScanner() {
+    await BarcodeScanner.showBackground();
+    document.querySelector('body').classList.remove('scanner-active');
   }
 
 }
