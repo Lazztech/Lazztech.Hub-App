35fd42768f205787745e03add9eda80280026bfa
Gian Lazzarini <gianlazzarini@gmail.com>
2023-01-07 17:12:24 -0800

    WIP PWA jsqr scanning implementation

--- src/app/pages/qr/qr.page.ts
+++ src/app/pages/qr/qr.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
 import { ScreenBrightness } from '@capacitor-community/screen-brightness';
@@ -12,6 +12,7 @@ import { ThemeService } from 'src/app/services/theme/theme.service';
 import { FileOpener } from '@capacitor-community/file-opener';
 import { isPlatform } from '@ionic/angular';
 import { EmailComposer } from '@awesome-cordova-plugins/email-composer/ngx';
+import jsQR from 'jsqr';
 
 @Component({
   selector: 'app-qr',
@@ -32,6 +33,16 @@ export class QrPage implements OnInit, OnDestroy {
   initialMode: 'show-code' | 'scan-code' = 'show-code';
   isHybrid: boolean = isPlatform('hybrid');
 
+  @ViewChild('video', { static: false }) video: ElementRef;
+  @ViewChild('canvas', { static: false }) canvas: ElementRef;
+  @ViewChild('fileinput', { static: false }) fileinput: ElementRef;
+
+  canvasElement: any;
+  videoElement: any;
+  canvasContext: any;
+  scanActive = false;
+  scanResult = null;
+
   constructor(
     private readonly themeService: ThemeService,
     private readonly router: Router,
@@ -51,13 +62,15 @@ export class QrPage implements OnInit, OnDestroy {
    }
 
   async ngOnInit() {
-    this.initialScreenBrightness = (await ScreenBrightness.getBrightness()).brightness;
+    if (this.isHybrid) {
+      this.initialScreenBrightness = (await ScreenBrightness.getBrightness()).brightness;
+    }
     switch (this.initialMode) {
       case 'show-code':
         await this.openCode()
         break;
       case 'scan-code':
-        await this.openScanner();
+        this.isHybrid ? await this.openScanner() : await this.startPwaScan();
         break;
       default:
         break;
@@ -69,7 +82,9 @@ export class QrPage implements OnInit, OnDestroy {
   }
 
   async ngOnDestroy() {
-    await ScreenBrightness.setBrightness({ brightness: this.initialScreenBrightness });
+    if (this.isHybrid) {
+      await ScreenBrightness.setBrightness({ brightness: this.initialScreenBrightness });
+    }
     await this.closeScanner();
   }
 
@@ -85,11 +100,11 @@ export class QrPage implements OnInit, OnDestroy {
     switch (event?.detail?.value) {
       case 'show-code':
         await this.openCode()
-        await this.closeScanner();
+        this.isHybrid ? await this.closeScanner() : this.stopPwaScan();
         break;
       case 'scan-code':
         await this.closeCode();
-        await this.openScanner();
+        this.isHybrid ? await this.openScanner() : await this.startPwaScan();
         break;
       default:
         break;
@@ -98,16 +113,114 @@ export class QrPage implements OnInit, OnDestroy {
 
   async openCode() {
     this.showCode = true;
-    await ScreenBrightness.setBrightness({ brightness: 1 });
+    await ScreenBrightness.setBrightness({ brightness: 1 }).catch(x => undefined);
   }
 
   async closeCode() {
     this.showCode = false;
-    await ScreenBrightness.setBrightness({ brightness: this.initialScreenBrightness });
+    await ScreenBrightness.setBrightness({ brightness: this.initialScreenBrightness }).catch(x => undefined);
   }
 
   async toggleTorch() {
     await BarcodeScanner.toggleTorch();
+  }
+
+  stopPwaScan() {
+    this.scanActive = false;
+    const stream = this.videoElement.srcObject;
+    const tracks = stream.getTracks();
+    tracks.forEach(function(track) {
+      track.stop();
+    });
+    
+    this.videoElement.srcObject = null;
+  }
+
+  async startPwaScan() {
+    console.log('here: ', this.showCode, this.isHybrid, this.canvas)
+    this.canvasElement = this.canvas.nativeElement;
+    this.canvasContext = this.canvasElement.getContext('2d');
+    this.videoElement = this.video.nativeElement;
+
+    // Not working on iOS standalone mode!
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: { facingMode: 'environment' }
+    });
+  
+    this.videoElement.srcObject = stream;
+    // Required for Safari
+    this.videoElement.setAttribute('playsinline', true);
+  
+    this.loading = true;
+
+    this.videoElement.play();
+    requestAnimationFrame(this.pwaScan.bind(this));
+  }
+  
+  async pwaScan() {
+    if (this.videoElement.readyState === this.videoElement.HAVE_ENOUGH_DATA) {
+      if (this.loading) {
+        this.loading = false;
+        this.loading = null;
+        this.scanActive = true;
+      }
+  
+      this.canvasElement.height = this.videoElement.videoHeight;
+      this.canvasElement.width = this.videoElement.videoWidth;
+  
+      this.canvasContext.drawImage(
+        this.videoElement,
+        0,
+        0,
+        this.canvasElement.width,
+        this.canvasElement.height
+      );
+      const imageData = this.canvasContext.getImageData(
+        0,
+        0,
+        this.canvasElement.width,
+        this.canvasElement.height
+      );
+      const code = jsQR(imageData.data, imageData.width, imageData.height, {
+        inversionAttempts: 'dontInvert'
+      });
+  
+      if (code) {
+        this.scanActive = false;
+        this.scanResult = code.data;
+        this.handleQrContent(code.data);
+      } else {
+        if (this.scanActive) {
+          requestAnimationFrame(this.pwaScan.bind(this));
+        }
+      }
+    } else {
+      requestAnimationFrame(this.pwaScan.bind(this));
+    }
+  }
+
+  handleFile(files: FileList) {
+    const file = files.item(0);
+  
+    var img = new Image();
+    img.onload = () => {
+      this.canvasContext.drawImage(img, 0, 0, this.canvasElement.width, this.canvasElement.height);
+      const imageData = this.canvasContext.getImageData(
+        0,
+        0,
+        this.canvasElement.width,
+        this.canvasElement.height
+      );
+      const code = jsQR(imageData.data, imageData.width, imageData.height, {
+        inversionAttempts: 'attemptBoth'
+      });
+  
+      if (code) {
+        this.scanResult = code.data;
+        this.handleQrContent(code.data);
+      }
+    };
+    img.src = URL.createObjectURL(file);
   }
 
   async openScanner() {
@@ -124,8 +237,7 @@ export class QrPage implements OnInit, OnDestroy {
 
     // if the result has content
     if (result.hasContent) {
-      alert(result.content);
-      console.log(result.content); // log the raw scanned content
+      this.handleQrContent(result.content)
     }
   }
 
@@ -133,6 +245,11 @@ export class QrPage implements OnInit, OnDestroy {
     await BarcodeScanner.showBackground();
     await BarcodeScanner.stopScan();
     document.querySelector('body').classList.remove('scanner-active');
+  }
+
+  async handleQrContent(content: any) {
+    alert(content);
+    console.log(content); // log the raw scanned content
   }
 
   async print() {
