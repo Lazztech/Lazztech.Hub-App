363d24df1c3e5d014764c348401acb95b2864b08
Gian Lazzarini <gianlazzarini@gmail.com>
2023-02-02 21:42:57 -0800

    Invite by hub or event (#63)
    * WIP
    
    * showing the filtered events & hubs to invite from
    
    * invite component ngOnDestroy
    
    * invite accordian group default to open
    
    * renaming variables
    
    * implemented event or hub filter selection
    
    * WIP implementing user filtering based on hub or event filter selection
    
    * added image field to user queries
    
    * filter out current user from invite lists
    
    * filterToValidToInvitePersons
    
    * retain checked status on people in the invite array, even when filter is changed
    
    * implemented select all toggle
    
    * fixed failing test

--- src/app/components/invite/invite.component.ts
+++ src/app/components/invite/invite.component.ts
@@ -1,14 +1,17 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges, ViewChild } from '@angular/core';
 import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Clipboard } from '@capacitor/clipboard';
 import { Share } from '@capacitor/share';
-import { NavController } from '@ionic/angular';
+import { IonAccordionGroup, NavController } from '@ionic/angular';
+import { QueryRef } from 'apollo-angular';
 import { NGXLogger } from 'ngx-logger';
+import { Subscription } from 'rxjs';
 import { InviteContext } from 'src/app/pages/qr/qr.page';
 import { AlertService } from 'src/app/services/alert/alert.service';
+import { AuthService } from 'src/app/services/auth/auth.service';
 import { HubService } from 'src/app/services/hub/hub.service';
-import { InviteUserToEventGQL, Scalars, User } from 'src/graphql/graphql';
+import { InviteUserToEventGQL, JoinUserEvent, JoinUserHub, Scalars, User, UserEventsGQL, UsersHubsGQL, UsersHubsQuery } from 'src/graphql/graphql';
 
 export enum InviteType {
   Hub = 'hub',
@@ -24,7 +27,7 @@ export type AlphabetMapOfUsers = {
   templateUrl: './invite.component.html',
   styleUrls: ['./invite.component.scss'],
 })
-export class InviteComponent implements OnInit, OnChanges {
+export class InviteComponent implements OnInit, OnDestroy, OnChanges {
 
   @Input() persons: Array<User> = [];
   @Input() inviteType: InviteType;
@@ -35,6 +38,9 @@ export class InviteComponent implements OnInit, OnChanges {
   @Input() qrSubtitle: string;
   @Input() qrImage: string;
   @Input() modal: any;
+  userHubs: UsersHubsQuery['usersHubs'];
+  queryRefs: QueryRef<any>[] = [];
+  subscriptions: Subscription[] = [];
 
   loading = false;
   allInvitesSucces = true;
@@ -43,6 +49,16 @@ export class InviteComponent implements OnInit, OnChanges {
   filter: string = '';
   filteredPersons: Array<User> = [];
   alphabetizedPersons: AlphabetMapOfUsers;
+  showPeopleFrom: 'all-people' | 'events-i-hosted' | 'events-im-invited-to' | 'hubs-im-an-admin-of' | 'hubs-im-a-member-of' = 'all-people';
+  hubsImAnAdminOf: Array<JoinUserHub> = [];
+  hubsImAMemberOf: Array<JoinUserHub> = [];
+  eventsIHosted: Array<JoinUserEvent> = [];
+  eventsImInvitedTo: Array<JoinUserEvent> = [];
+  userId: any;
+  eventFilter: JoinUserEvent;
+  hubFilter: JoinUserHub;
+  @ViewChild('filterAccordionGroup', { static: true }) filterAccordianGroup: IonAccordionGroup;
+  selectAll: boolean = false;
 
   get email() {
     return this.myForm.get('email');
@@ -56,9 +72,12 @@ export class InviteComponent implements OnInit, OnChanges {
     private readonly logger: NGXLogger,
     private readonly navCtrl: NavController,
     private readonly inviteUserToEventService: InviteUserToEventGQL,
+    private readonly userHubsGQLService: UsersHubsGQL,
+    private readonly userEvents: UserEventsGQL,
+    private readonly authService: AuthService,
   ) { }
 
-  ngOnInit() {
+  async ngOnInit() {
     this.id = this.route.snapshot.paramMap.get('id');
     this.myForm = this.fb.group({
       email: ['', [
@@ -67,12 +86,42 @@ export class InviteComponent implements OnInit, OnChanges {
       ]],
     });
     this.alphabetizedPersons = this.alphabetizePersons(
-      this.persons
+      this.filterToValidToInvitePersons(this.persons)
+    );
+    const userHubsRef = this.userHubsGQLService.watch();
+    const userEventsRef = this.userEvents.watch();
+    this.queryRefs.push(
+      userHubsRef,
+      userEventsRef
+    );
+
+    this.userId = (await this.authService.user())?.id;
+
+    this.subscriptions.push(
+      userHubsRef.valueChanges.subscribe(result => {
+        const userHubs = result?.data?.usersHubs;
+        this.hubsImAnAdminOf = userHubs?.filter(x => x?.isOwner) as JoinUserHub[];
+        this.hubsImAMemberOf = userHubs?.filter(x => !x?.isOwner) as JoinUserHub[];
+      }),
+      userEventsRef.valueChanges.subscribe(result => {
+        const userEvents = result?.data?.usersEvents;
+        this.eventsIHosted = userEvents.filter(x => {
+          return x?.event?.createdBy?.id === this.userId
+        }) as JoinUserEvent[];
+        this.eventsImInvitedTo = userEvents.filter(x => {
+          return x?.event?.createdBy?.id !== this.userId
+        }) as JoinUserEvent[];
+      }),
     );
   }
 
   ngOnChanges(changes: SimpleChanges): void {
     console.log(changes);
+  }
+
+  ngOnDestroy(): void {
+    this.queryRefs?.forEach(x => x.stopPolling());
+    this.subscriptions?.forEach(x => x.unsubscribe());
   }
 
   async copyShareLink() {
@@ -89,13 +138,38 @@ export class InviteComponent implements OnInit, OnChanges {
     });
   }
 
-  checkboxChanged(person) {
+  showPeopleFromChanged(ev) {
+    this.showPeopleFrom = ev.target.value;
+    this.eventFilter = undefined;
+    this.hubFilter = undefined;
+    if (this.showPeopleFrom !== 'all-people') {
+      this.filterAccordianGroup.value = ['first'];
+    } else {
+      this.alphabetizedPersons = this.alphabetizePersons(
+        this.filterToValidToInvitePersons(this.persons)
+      );
+      this.filterAccordianGroup.value = undefined;
+    }
+  }
+
+  checkboxChanged(person, ev) {
     const invitee = { name: person.firstName, email: person.email };
-    if (this.invites.filter(x => x.email === invitee.email).length) {
+    if (!ev.detail.checked && this.invites.some(x => x.email === invitee.email)) {
       const i = this.invites.findIndex(x => x.email === invitee.email);
       this.invites.splice(i, 1);
-    } else {
+    } else if (ev.detail.checked && !this.invites.some(x => x.email === invitee.email)) {
       this.invites.push(invitee);
+    }
+  }
+
+  isChecked(person) {
+    return this.invites.some(x => x.email === person?.email);
+  }
+
+  toggleSelectAll() {
+    this.selectAll = !this.selectAll;
+    if (!this.selectAll) {
+      this.invites = [];
     }
   }
 
@@ -156,16 +230,46 @@ export class InviteComponent implements OnInit, OnChanges {
   }
 
   personIsFiltered(person: User) {
-    if (this.filter && this.filter?.trim() !== '') {
+    if (this.showPeopleFrom !== 'all-people' && this.eventFilter) {
+      return !this.eventFilter.event.usersConnection?.some(x => x.userId === person.id);
+    } else if (this.showPeopleFrom !== 'all-people' && this.hubFilter) {
+      return !this.hubFilter.hub.usersConnection?.some(x => x.userId === person.id);
+    } else if (this.filter && this.filter?.trim() !== '') {
       const name = person?.firstName?.trim()?.toLowerCase() + person?.lastName?.trim()?.toLowerCase();
       return !name.includes(this.filter?.trim().toLowerCase());
+    } else {
+      return false;
     }
+  }
 
-    return false;
+  eventFilterClicked(userEvent: JoinUserEvent) {
+    this.eventFilter = userEvent;
+    this.alphabetizedPersons = this.alphabetizePersons(
+      this.filterToValidToInvitePersons(userEvent?.event?.usersConnection?.map(x => x.user))
+    );
+    this.filterAccordianGroup.value = undefined;
+  }
+
+  hubFilterClicked(userHub: JoinUserHub) {
+    this.hubFilter = userHub;
+    this.alphabetizedPersons = this.alphabetizePersons(
+      this.filterToValidToInvitePersons(userHub?.hub?.usersConnection?.map(x => x.user))
+    );
+    this.filterAccordianGroup.value = undefined;
   }
 
   async filterPeople(ev: any) {
     this.filter = ev?.target?.value;
+  }
+
+  filterToValidToInvitePersons(persons: Array<User>): Array<User> {
+    // remove this user as they shouldn't be able to invite themselves
+    const personsWithOutThisUser = persons.filter(x => x.id !== this.userId);
+    // assuming the input persons are only those that haven't been invited already
+    // they're the only valid options to show so we filter the input down to them
+    return personsWithOutThisUser.filter(
+      x => this.persons.some(y => y.id === x.id)
+    );
   }
 
   alphabetizePersons(persons: Array<User>): AlphabetMapOfUsers {
