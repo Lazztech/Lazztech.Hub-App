bb7e3f876e9ea6a769837854a958098b6727b878
Gian Lazzarini <gianlazzarini@gmail.com>
2023-04-14 23:50:54 -0700

    animated pulsing dot

--- src/app/components/maplibre/maplibre.component.ts
+++ src/app/components/maplibre/maplibre.component.ts
@@ -34,6 +34,12 @@ export class MaplibreComponent implements OnChanges, AfterViewInit {
     public navCtrl: NavController,
   ) { }
 
+  size = 100;
+
+  // implementation of StyleImageInterface to draw a pulsing dot icon on the map
+  // see https://maplibre.org/maplibre-gl-js-docs/api/properties/#styleimageinterface for more info
+  pulsingDot: any;
+
   ngOnChanges(changes: SimpleChanges): void {
     if (this.map && changes.center) {
       this.setCenter();
@@ -79,6 +85,33 @@ export class MaplibreComponent implements OnChanges, AfterViewInit {
       }));
 
       this.rotateCamera(0, this.map);
+      this.pulsingDot = this.createPulsingDot(this.map, this.size);
+
+      this.map.addImage('pulsing-dot', this.pulsingDot, { pixelRatio: 2 });
+
+      this.map.addSource('points', {
+        'type': 'geojson',
+        'data': {
+          'type': 'FeatureCollection',
+          'features': [
+            {
+              'type': 'Feature',
+              'geometry': {
+                'type': 'Point',
+                'coordinates': [this.center.longitude, this.center.latitude]
+              }
+            }
+          ]
+        }
+      });
+      this.map.addLayer({
+        'id': 'points',
+        'type': 'symbol',
+        'source': 'points',
+        'layout': {
+          'icon-image': 'pulsing-dot'
+        }
+      });
     });
   }
 
@@ -95,5 +128,71 @@ export class MaplibreComponent implements OnChanges, AfterViewInit {
       this.map.setCenter({ lat: this.center.latitude, lon: this.center.longitude }, 12);
     }
   }
+
+  createPulsingDot = (map: Map, size: number) => ({
+    width: size,
+    height: size,
+    data: new Uint8Array(size * size * 4),
+
+    // get rendering context for the map canvas when layer is added to the map
+    onAdd: function () {
+      var canvas = document.createElement('canvas');
+      canvas.width = size;
+      canvas.height = size;
+      this.context = canvas.getContext('2d');
+    },
+
+    // called once before every frame where the icon will be used
+    render: function () {
+      var duration = 1000;
+      var t = (performance.now() % duration) / duration;
+
+      var radius = (size / 2) * 0.3;
+      var outerRadius = (size / 2) * 0.7 * t + radius;
+      var context = this.context;
+
+      // draw outer circle
+      context.clearRect(0, 0, size, size);
+      context.beginPath();
+      context.arc(
+        size / 2,
+        size / 2,
+        outerRadius,
+        0,
+        Math.PI * 2
+      );
+      context.fillStyle = 'rgba(255, 200, 200,' + (1 - t) + ')';
+      context.fill();
+
+      // draw inner circle
+      context.beginPath();
+      context.arc(
+        size / 2,
+        size / 2,
+        radius,
+        0,
+        Math.PI * 2
+      );
+      context.fillStyle = 'rgba(255, 100, 100, 1)';
+      context.strokeStyle = 'white';
+      context.lineWidth = 2 + 4 * (1 - t);
+      context.fill();
+      context.stroke();
+
+      // update this image's data with data from the canvas
+      this.data = context.getImageData(
+        0,
+        0,
+        size,
+        size
+      ).data;
+
+      // continuously repaint the map, resulting in the smooth animation of the dot
+      map.triggerRepaint();
+
+      // return `true` to let the map know that the image was updated
+      return true;
+    }
+  });
 
 }
