c287d7aa8c7a9aaad73e054936b962d19067508d
Gian Lazzarini <gianlazzarini@gmail.com>
2023-04-17 20:05:43 -0700

    default href onn map page back button

--- src/app/pages/map/map.page.ts
+++ src/app/pages/map/map.page.ts
@@ -1,8 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ApolloQueryResult } from '@apollo/client/core';
 import { QueryRef } from 'apollo-angular';
 import { Subscription } from 'rxjs';
-import { Hub, UserEventsGQL, UserEventsQuery, UsersHubsGQL, UsersHubsQuery } from '../../../graphql/graphql';
+import { Hub, UsersHubsGQL, UsersHubsQuery } from '../../../graphql/graphql';
 import { LocationService } from '../../services/location/location.service';
 
 @Component({
@@ -18,51 +17,24 @@ export class MapPage implements OnInit, OnDestroy {
   locations: Array<any>;
 
   userHubs: UsersHubsQuery['usersHubs'];
-  userEventsQueryResult: ApolloQueryResult<UserEventsQuery>;
-  filteredEvents: UserEventsQuery['usersEvents'];
-  sortedEvents: UserEventsQuery['usersEvents'];
-  upcomingEvents: UserEventsQuery['usersEvents'];
-  elapsedEvents: UserEventsQuery['usersEvents'];
 
   constructor(
     public locationService: LocationService,
     private readonly userHubsGQLService: UsersHubsGQL,
-    private readonly userEvents: UserEventsGQL,
   ) {}
 
   ngOnInit(): void {
     const userHubsQueryRef = this.userHubsGQLService.watch(null, { pollInterval: 3000 });
-    // const userEventsQueryRef = this.userEvents.watch(null, { pollInterval: 3000 });
     this.queryRefs.push(
       userHubsQueryRef,
-      // userEventsQueryRef
     );
 
     this.subscriptions.push(
       userHubsQueryRef.valueChanges.subscribe(x => {
         this.loading = x.loading;
         this.userHubs = [...x?.data?.usersHubs]?.filter(x => x.hub);
-        this.locations = [...this.userHubs?.map(x => x.hub as Hub), ...this.upcomingEvents?.filter(x => x.event)?.map(x => x.event)];
+        this.locations = this.userHubs?.map(x => x.hub as Hub);
       }),
-      // userEventsQueryRef?.valueChanges?.subscribe(result => {
-      //   this.userEventsQueryResult = result;
-      //   this.loading = result.loading;
-      //   if (this.userEventsQueryResult?.data?.usersEvents) {
-      //     this.sortedEvents = [...this.userEventsQueryResult?.data?.usersEvents]?.sort(
-      //       (a, b) => new Date(b?.event?.startDateTime).valueOf() - new Date(a?.event?.startDateTime).valueOf()
-      //     );
-      //     this.upcomingEvents = this.sortedEvents?.filter(userEvents => (
-      //       new Date().valueOf() <= new Date(userEvents?.event?.startDateTime).valueOf()
-      //     )).sort(
-      //       (a, b) => new Date(a?.event?.startDateTime).valueOf() - new Date(b?.event?.startDateTime).valueOf()
-      //     );
-      //     this.elapsedEvents = this.sortedEvents?.filter(userEvents => (
-      //       new Date().valueOf() > new Date(userEvents?.event?.startDateTime).valueOf()
-      //     ));
-
-      //     this.locations = [...this.userHubs?.map(x => x.hub as Hub), ...this.upcomingEvents?.filter(x => x.event)?.map(x => x.event)];
-      //   }
-      // })
     )
   }
 
