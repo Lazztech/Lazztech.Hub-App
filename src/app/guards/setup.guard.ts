77b0f4ed08f1f497065dc576fd340550f4230391
Gian Lazzarini <gianlazzarini@gmail.com>
2023-06-03 12:21:18 -0700

    adjusted error handling in setup guard

--- src/app/guards/setup.guard.ts
+++ src/app/guards/setup.guard.ts
@@ -3,6 +3,7 @@ import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { AuthService } from '../services/auth/auth.service';
 import { GeofenceService } from '../services/geofence/geofence.service';
 import { NotificationsService } from '../services/notifications/notifications.service';
+import { ErrorService } from '../services/error.service';
 
 @Injectable({
   providedIn: 'root'
@@ -13,35 +14,36 @@ export class SetupGuard  {
     private authService: AuthService,
     private notificationsService: NotificationsService,
     private geofenceService: GeofenceService,
+    private errorService: ErrorService
   ) { }
 
   async canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Promise<boolean> {
-    const isAuthed = await this.authService.getToken();
-    // user is signed in
-    if (isAuthed) {
-      try {
-        // setup background geolocation
-        await this.geofenceService.configureBackgroundGeolocation();
-        let state = await this.geofenceService.ready();
-        if (!state.enabled) {
-          state = await this.geofenceService.start();
-        }
-        // hydrate hub geofences
-        // FIXME: Should this be updated on polling of users hubs?
-        // Also is this why I get recurring notifications about entering a hub that I'm
-        // already at, because this is firing in the background every time the app starts
-        // up for the backgroundgeolocation plugin???
-        this.geofenceService.syncGeofences()
-          .then(() => console.log('done with geofenceService.syncGeofences()')); 
+    try {
+      const isAuthed = await this.authService.getToken();
+      // user is signed in
+      if (isAuthed) {
+          // setup background geolocation
+          await this.geofenceService.configureBackgroundGeolocation();
+          let state = await this.geofenceService.ready();
+          if (!state.enabled) {
+            state = await this.geofenceService.start();
+          }
+          // hydrate hub geofences
+          // FIXME: Should this be updated on polling of users hubs?
+          // Also is this why I get recurring notifications about entering a hub that I'm
+          // already at, because this is firing in the background every time the app starts
+          // up for the backgroundgeolocation plugin???
+          this.geofenceService.syncGeofences()
+            .then(() => console.log('done with geofenceService.syncGeofences()')); 
 
-        // permissions
-        this.notificationsService.setupPushNotifications()
-          .then(() => console.log('done with notificationsService.setupPushNotifications()'));
-      } catch (error) {
-        
+          // permissions
+          this.notificationsService.setupPushNotifications()
+            .then(() => console.log('done with notificationsService.setupPushNotifications()'));
       }
+    } catch (error) {
+      this.errorService.handleError(error);
     }
 
     return true;
