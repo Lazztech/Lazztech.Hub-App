d9255822b9896d81e17de7356a2c29e963d6a1f5
Gian Lazzarini <gianlazzarini@gmail.com>
2023-06-04 13:38:49 -0700

    fixed test

--- src/app/services/debugger/debugger.service.spec.ts
+++ src/app/services/debugger/debugger.service.spec.ts
@@ -1,9 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { DebuggerService } from './debugger.service';
+import { LoggerModule } from 'ngx-logger';
+import { environment } from 'src/environments/environment';
 
 describe('DebuggerService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      LoggerModule.forRoot(environment.logging),
+    ]
+  }));
 
   it('should be created', () => {
     const service: DebuggerService = TestBed.get(DebuggerService);
