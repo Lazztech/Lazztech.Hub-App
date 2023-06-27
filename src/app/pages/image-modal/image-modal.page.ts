03e49c3e34eea92f4e173808aba9d6def472d1fa
Gian Lazzarini <gianlazzarini@gmail.com>
2023-06-26 17:04:25 -0700

    WIP presentActionSheet for upload

--- src/app/pages/image-modal/image-modal.page.ts
+++ src/app/pages/image-modal/image-modal.page.ts
@@ -1,5 +1,6 @@
 import { Component, Input, ViewChild } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { ActionSheetController, ModalController } from '@ionic/angular';
+import { Action } from 'rxjs/internal/scheduler/Action';
 import { File, JoinHubFile } from 'src/graphql/graphql';
 import { SwiperOptions } from 'swiper';
 import SwiperCore, { Zoom } from 'swiper';
@@ -23,6 +24,7 @@ export class ImageModalPage {
   };
   constructor(
     private modalCtl: ModalController,
+    private actionSheetController: ActionSheetController,
   ) {}
 
   close() {
@@ -32,6 +34,41 @@ export class ImageModalPage {
   zoom(zoomIn: boolean) {
     const zoom = this.swiper.swiperRef.zoom;
     zoomIn ? zoom.in() : zoom.out(); 
+  }
+
+  async presentActionSheet() {
+    if (this.file) {
+      const buttons = [];   
+        buttons.push(
+        {
+          text: 'Report as Inappropriate',
+          role: 'destructive',
+          // handler: () => {
+          //   if (confirm('Report as Inappropriate? This may result in the removal of data & the offending content creator.')) {
+          //     this.loading = true;
+          //     this.hubService.reportAsInappropriate(this.id).then(() => {
+          //       this.loading = false;
+          //       this.navCtrl.back();
+          //     });
+          //   }
+          // }
+        }
+      );
+
+      const actionSheet = await this.actionSheetController.create({
+        header: 'Upload Options',
+        buttons: [
+          ...buttons,
+          {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            // this.logger.log('Cancel clicked');
+          }
+        }]
+      });
+      await actionSheet.present();
+    }
   }
 
 }
