314ab3e69b562dda0e6a34d4a679b72549164764
Gian Lazzarini <gianlazzarini@gmail.com>
2023-07-04 12:59:29 -0700

    WIP manage uploads page

--- src/app/pages/manage-uploads/manage-uploads.page.ts
+++ src/app/pages/manage-uploads/manage-uploads.page.ts
@@ -1,11 +1,9 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { FormGroup, UntypedFormGroup } from '@angular/forms';
-import { Router } from '@angular/router';
-import { GalleryPhotos, Photo } from '@capacitor/camera';
 import { IonRouterOutlet, ModalController, NavController } from '@ionic/angular';
 import { NGXLogger } from 'ngx-logger';
 import { Subscription } from 'rxjs';
 import { LocationService } from 'src/app/services/location/location.service';
+import { MyFileUploadsGQL, MyFileUploadsQuery } from 'src/graphql/graphql';
 import { ImageModalPage } from '../image-modal/image-modal.page';
 
 @Component({
@@ -16,34 +14,24 @@ import { ImageModalPage } from '../image-modal/image-modal.page';
 export class ManageUploadsPage implements OnInit, OnDestroy {
 
   loading = false;
-  myForm: UntypedFormGroup;
-  image: any[] = [];
-  photos: Photo[] = [];
-  gallery: GalleryPhotos;
+  data: MyFileUploadsQuery['me']['fileUploads'];
   subscriptions: Subscription[] = [];
-
-  seed: any;
-  seedType: 'hub' | 'event';
 
   constructor(
     private readonly logger: NGXLogger,
-    private readonly router: Router,
     public readonly navCtrl: NavController,
     public readonly routerOutlet: IonRouterOutlet,
     public readonly locationService: LocationService,
     private readonly modalCtl: ModalController,
-  ) {
-    this.seed = this.router.getCurrentNavigation()?.extras?.state?.seed;
-    this.seedType = this.router.getCurrentNavigation()?.extras?.state?.seedType;
-   }
+    private readonly myFileUploadsGqlService: MyFileUploadsGQL,
+  ) { }
 
   async openPreview(startingFileIndex) {
-    const files = this.seed?.fileUploads?.map(x => x?.file) || this.seed?.event?.fileUploads?.map(x => x?.file);
     const modal = await this.modalCtl.create({
       component: ImageModalPage,
       componentProps: {
         startingFileIndex,
-        files,
+        files: this.data,
       },
       cssClass: 'transparent-modal fullscreen'
     })
@@ -51,20 +39,19 @@ export class ManageUploadsPage implements OnInit, OnDestroy {
   }
 
   async ngOnInit() {
-    this.myForm = new FormGroup({});
+    const myUploadsQueryRef = this.myFileUploadsGqlService.fetch(null, {
+      fetchPolicy: 'cache-first'
+    });
+
+    this.subscriptions.push(
+      myUploadsQueryRef.subscribe(result => {
+        this.data = result.data.me.fileUploads;
+      }),
+    )
   }
 
   ngOnDestroy(): void {
     this.subscriptions.forEach(x => x.unsubscribe());
-  }
-
-  goToUploadPage() {
-    this.navCtrl.navigateForward('upload', {
-      state: {
-        seed: this.seed,
-        seedType: this.seedType,
-      }
-    });
   }
 
   goToPersonPage(id: number, user: any) {
