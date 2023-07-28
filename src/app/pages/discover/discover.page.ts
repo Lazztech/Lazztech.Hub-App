1eba93233ef4a0fdd0d2e2a20b02bdb95f5242e4
Gian Lazzarini <gianlazzarini@gmail.com>
2023-07-27 21:34:11 -0700

    clean up discover page

--- src/app/pages/discover/discover.page.ts
+++ src/app/pages/discover/discover.page.ts
@@ -1,10 +1,9 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ActionSheetButton, ActionSheetController, IonRouterOutlet, ModalController, NavController } from '@ionic/angular';
+import { IonRouterOutlet, ModalController, NavController } from '@ionic/angular';
 import { NGXLogger } from 'ngx-logger';
 import { Subscription } from 'rxjs';
-import { AuthService } from 'src/app/services/auth/auth.service';
 import { LocationService } from 'src/app/services/location/location.service';
-import { DeleteFileByIdGQL, DiscoverGQL, DiscoverQuery, EventDocument, File, HubDocument, ReportFileAsInappropriateGQL, User } from 'src/graphql/graphql';
+import { DiscoverGQL } from 'src/graphql/graphql';
 import { ImageModalPage } from '../image-modal/image-modal.page';
 
 @Component({
@@ -28,10 +27,6 @@ export class DiscoverPage implements OnInit, OnDestroy {
     public readonly routerOutlet: IonRouterOutlet,
     public readonly locationService: LocationService,
     private readonly modalCtl: ModalController,
-    private actionSheetCtrl: ActionSheetController,
-    private readonly authService: AuthService,
-    private readonly reportFileAsInappropriateGqlService: ReportFileAsInappropriateGQL,
-    private readonly deleteFileByIdGqlService: DeleteFileByIdGQL,
     private readonly discoverGraphQLService: DiscoverGQL,
   ) {}
 
@@ -86,79 +81,6 @@ export class DiscoverPage implements OnInit, OnDestroy {
         user
       }
     });
-  }
-
-  isOwner(user: User): boolean {
-    // if (this.seedType === 'event') {
-    //   return this.userEvent?.event?.createdBy?.id === user.id;
-    // } else if (this.seedType === 'hub') {
-    //   return this.userHub?.isOwner;
-    // }
-    return false;
-  }
-
-  async presentActionSheet(file: File) {
-    const user = await this.authService.user();
-    const buttons: ActionSheetButton<any>[] = [];
-    if (file?.createdBy?.id === user?.id || this.isOwner(user)) {
-        buttons.push(
-          {
-            text: 'Delete',
-            role: 'destructive',
-            handler: () => {
-              if (confirm('Are you sure you want to delete this?')) {
-                this.loading = true;
-                this.deleteFileByIdGqlService.mutate({ fileId: file.id }, {
-                  refetchQueries: [
-                    { 
-                      query: this.seedType == 'event' ? EventDocument : HubDocument,
-                      variables: { id: this.seedId }
-                    }
-                  ],
-                  awaitRefetchQueries: true,
-                }).toPromise().then(() => {
-                  this.loading = false;
-                });
-              }
-            }
-          }
-        )
-    } else {
-      buttons.push(
-        {
-          text: 'Report as Inappropriate',
-          role: 'destructive',
-          handler: () => {
-            if (confirm('Report as Inappropriate? This may result in the removal of data & the offending content creator.')) {
-              this.loading = true;
-              this.reportFileAsInappropriateGqlService.mutate({ fileId: file.id}, {
-                refetchQueries: []
-              }).toPromise().then(() => {
-                this.loading = false;
-                this.navCtrl.back();
-              });
-            }
-          }
-        }
-      )
-    }
-
-    buttons.push(
-      {
-        text: 'Cancel',
-        role: 'cancel',
-        data: {
-          action: 'cancel',
-        },
-      }
-    );
-
-    const actionSheet = await this.actionSheetCtrl.create({
-      header: 'File Actions',
-      buttons,
-    });
-
-    await actionSheet.present();
   }
 
 }
