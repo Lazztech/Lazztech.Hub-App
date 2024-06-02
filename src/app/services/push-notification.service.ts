d0cbcb0621eef3069cb5726ddc6cf57ba8325e49
Gian Lazzarini <gianlazzarini@gmail.com>
2024-06-01 17:06:05 -0700

    push notification service

--- src/app/services/push-notification.service.ts
+++ src/app/services/push-notification.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { SwPush } from '@angular/service-worker';
+import { Storage } from '@ionic/storage-angular';
 import { NGXLogger } from 'ngx-logger';
 import { environment } from 'src/environments/environment';
 import { AddUserWebPushNotificationSubscriptionGQL } from 'src/graphql/graphql';
@@ -13,6 +14,7 @@ export class PushNotificationService {
   constructor(
     private readonly logger: NGXLogger,
     private swPush: SwPush,
+    private storage: Storage,
     private addUserWebPushNotificationSubscriptionGQLService: AddUserWebPushNotificationSubscriptionGQL,
   ) {}
 
@@ -22,12 +24,12 @@ export class PushNotificationService {
         .requestSubscription({
           serverPublicKey: environment.webPushPublicVapidKey
         })
-        .then((sub: PushSubscription) => {
+        .then(async (sub: PushSubscription) => {
           // Save the subscription object to your server
-          this.saveSubscription(sub);
+          await this.saveSubscription(sub);
 
           // Store the subscription in local storage or any other storage mechanism
-          this.storeSubscription(sub);
+          await this.storeSubscription(sub);
 
           console.log('Display', JSON.stringify(sub)); // This will be required for the Nest.js backend to send notifications
         })
@@ -70,8 +72,8 @@ export class PushNotificationService {
     return response;
   }
 
-  private storeSubscription(sub: PushSubscription): void {
+  private async storeSubscription(sub: PushSubscription) {
     // Store the subscription in local storage or any other storage mechanism
-
+    await this.storage.set('webPushSubcription', sub);
   }
 }
