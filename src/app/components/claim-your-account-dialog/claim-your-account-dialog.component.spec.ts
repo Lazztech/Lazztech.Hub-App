2706cdbd21c7bec9aeb67885f1f88a6c530a4aff
Gian Lazzarini <gianlazzarini@gmail.com>
2024-06-10 20:15:57 -0700

    passing tests

--- src/app/components/claim-your-account-dialog/claim-your-account-dialog.component.spec.ts
+++ src/app/components/claim-your-account-dialog/claim-your-account-dialog.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ClaimYourAccountDialogComponent } from './claim-your-account-dialog.component';
+import { IonicStorageModule, Storage } from '@ionic/storage-angular';
+import { ApolloTestingModule } from 'apollo-angular/testing';
+import { LoggerModule } from 'ngx-logger';
+import { environment } from 'src/environments/environment';
 
 describe('ClaimYourAccountDialogComponent', () => {
   let component: ClaimYourAccountDialogComponent;
@@ -8,9 +12,16 @@ describe('ClaimYourAccountDialogComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ClaimYourAccountDialogComponent ]
+      declarations: [ ClaimYourAccountDialogComponent ],
+      imports: [
+        IonicStorageModule.forRoot(),
+        ApolloTestingModule,
+        LoggerModule.forRoot(environment.logging),
+      ]
     })
     .compileComponents();
+    const storage: Storage = TestBed.get(Storage);
+    await storage.create();
 
     fixture = TestBed.createComponent(ClaimYourAccountDialogComponent);
     component = fixture.componentInstance;
