7f63a8b75f4871a9b040e9ba4893489e2a223ca5
shoshannaTM <sctm14@gmail.com>
2025-10-10 13:31:28 -0700

    fixed test failure

--- src/app/components/profile/profile.component.spec.ts
+++ src/app/components/profile/profile.component.spec.ts
@@ -1,7 +1,11 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-
+import { ApolloTestingModule } from 'apollo-angular/testing';
 import { ProfileComponent } from './profile.component';
+import { IonicStorageModule } from '@ionic/storage-angular';
+import { environment } from 'src/environments/environment';
+import { LoggerModule } from 'ngx-logger';
+import { DirectivesModule } from 'src/app/directives/directives.module';
 
 describe('ProfileComponent', () => {
   let component: ProfileComponent;
@@ -11,6 +15,12 @@ describe('ProfileComponent', () => {
     TestBed.configureTestingModule({
       declarations: [ ProfileComponent ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      imports: [
+        IonicStorageModule.forRoot(),
+        ApolloTestingModule,
+        LoggerModule.forRoot(environment.logging),
+        DirectivesModule,
+      ],
     })
     .compileComponents();
   }));
