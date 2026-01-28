5541ea65834d169e606d3a89fa2d45b004003e1a
shoshannaTM <sctm14@gmail.com>
2026-01-28 14:25:30 -0800

    basic user flow passing in chromium

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,6 +13,7 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests',
+  timeout: 60000,
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -26,7 +27,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: 'http://localhost:4200',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -36,7 +37,12 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: {
+        ...devices['Desktop Chrome'],
+        launchOptions: {
+          args: ['--enable-features=SharedArrayBuffer', '--use-gl=angle', '--use-angle=gl'],
+        },
+      },
     },
 
     {
@@ -71,9 +77,11 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'npm run start',
+    url: 'http://localhost:4200',
+    reuseExistingServer: !process.env.CI,
+    stderr: "pipe",
+    stdout: "pipe"
+  },
 });
---
+++ tests/basic-user-flow.spec.ts
@@ -0,0 +1,45 @@
+import { test, expect } from '@playwright/test';
+
+test('create-profile-and-create-event', async ({ page, context }) => {
+  // Grant geolocation permission and set a default location
+  await context.grantPermissions(['geolocation']);
+  await context.setGeolocation({ latitude: 48.8566, longitude: 2.3522 }); // Paris coordinates
+  
+  // Generate unique email using Unix timestamp in milliseconds
+  const timestamp = Date.now();
+  const uniqueEmail = `playwright_test_${timestamp}@email.com`;
+
+  await page.goto('http://localhost:4200/landing?returnUrl=%2Ftabs%2Fhome');
+  await page.getByRole('button', { name: 'Continue As Guest' }).click();
+  await expect(page.locator('app-home')).toContainText('Set your name and claim your account! This ensures you\'re able to recover your account if signed out.');
+  await page.getByText('Set your name and claim your').click();
+  await page.locator('#ion-input-2').click();
+  await page.locator('#ion-input-2').fill('playwright_test');
+  await page.locator('#ion-input-3').click();
+  await page.locator('#ion-input-3').fill('test');
+  await page.locator('#ion-input-4').click();
+  await page.locator('#ion-input-4').fill(uniqueEmail);
+  await page.locator('#ion-input-7').click();
+  await page.locator('#ion-input-7').fill('playwright_test');
+  await page.getByRole('button', { name: 'Save' }).click();
+  await expect(page.locator('ion-tabs')).toContainText('playwright_test test');
+  await page.locator('#tab-button-home > .ios > .icon-inner > .ionicon').click();
+  await page.locator('ion-fab-button > .ios > .icon-inner > .ionicon').first().click();
+  await page.locator('circle').first().click();
+  await page.getByRole('textbox', { name: '*' }).click();
+  await page.getByRole('textbox', { name: '*' }).fill('playwright_test_event');
+  await page.locator('input[name="ion-input-11"]').click();
+  await page.locator('input[name="ion-input-11"]').fill('0');
+  await page.locator('input[name="ion-input-12"]').click();
+  await page.locator('input[name="ion-input-12"]').fill('2');
+  await page.locator('#ion-textarea-0').click();
+  await page.locator('#ion-textarea-0').fill('description of event');
+  await page.locator('#ion-input-13').click();
+  await page.getByRole('searchbox', { name: 'search text' }).click();
+  await page.getByRole('searchbox', { name: 'search text' }).fill('paris');
+  await page.waitForTimeout(500);
+  await page.getByText(/france/i).first().click({ force: true });
+  await page.getByRole('button', { name: 'Select Location' }).click();
+  await page.getByRole('button', { name: 'Create' }).click();
+  await expect(page.locator('ion-card-title')).toContainText('playwright_test_event');
+});
