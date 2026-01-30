ebaaa4c286012c8aa79591349e87024d657d6bca
shoshannaTM <sctm14@gmail.com>
2026-01-30 15:12:08 -0800

    addressed pr requests

--- playwright.config.ts
+++ playwright.config.ts
@@ -78,7 +78,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run start:local',
+    command: 'npm run start',
     url: 'http://localhost:4200',
     reuseExistingServer: !process.env.CI,
     stderr: "pipe",
